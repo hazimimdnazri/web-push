06fdb7dea2403dcef0b0dc7b69802b16fde03d35
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-03-29 03:55:54 +0800

    GENESIS

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const webpush = require('web-push');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "client")))
+
+app.use(cors({
+    origin: '*',
+    methods: ['POST', 'GET'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const publicVapidKey = process.env.VAPID_PUBLIC_KEY;
 const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
