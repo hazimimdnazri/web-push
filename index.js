99b8b44ba23dbd7854d9d34f180b780f5926bcf1
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-03-29 03:40:47 +0800

    GENESIS

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const webpush = require('web-push');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client")))
+
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY;
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
+
+webpush.setVapidDetails("mailto:zimy@projeklah.com", publicVapidKey, privateVapidKey);
+
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
+    webpush.sendNotification(subscription, payload).catch(console.log);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server started on port " + process.env.PORT);
+})
