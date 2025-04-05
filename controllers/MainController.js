cbe45f96f28a7008f0d7a7a5ad19fc2c1b006f68
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-04-06 02:15:36 +0800

    Cleaning.

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -2,8 +2,11 @@ const Logger = require('../services/Logger');
 const logger = Logger.getInstance();
 
 const healthCheck = (req, res) => {
-    logger.info("Health check from " + req.socket.remoteAddress);
-    res.status(200).json({ message: 'OK' });
+    logger.info("Health check");
+    res.status(200).json({
+        status: 'OK',
+        message: 'Server is healthy'
+    });
 };
 
 module.exports = {
