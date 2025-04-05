cc038744081201c918124cae00a1c6b0bccab7ac
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-04-06 04:43:37 +0800

    123

--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -3,7 +3,7 @@ const Logger = require('../services/Logger');
 const logger = Logger.getInstance();
 
 const subscribe = (req, res) => {
-    logger.info("Subscription request received", req.body);
+    logger.info("Subscription request received");
 
     const subscription = req.body;
     res.status(201).json({});
@@ -18,21 +18,11 @@ const subscribe = (req, res) => {
     });
 
     webpush.sendNotification(subscription, payload).catch(console.log);
-    setTimeout(() => {
-        webpush.sendNotification(subscription, JSON.stringify({
-            title: "Testing Alert",
-            body: "This is just a test alert.",
-            icon: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg",
-            actions: [
-                { action: "open_url", title: "Open URL" },
-                { action: "dismiss", title: "Dismiss" }
-            ]
-        })).catch(console.log);
-    }, 3000);
 };
 
-const alert = (subscription) => {
+const alert = (req, res) => {
     logger.info("Alert request received");
+    const subscription = req.body;
     const payload = JSON.stringify({
         title: "Testing Alert",
         body: "This is just a test alert.",
