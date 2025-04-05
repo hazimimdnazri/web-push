ad26520944aa61240f1ae2ad06fc471715d81fc5
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2025-04-06 01:33:44 +0800

    Cleaning.

--- route.js
+++ route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('./controllers/NotificationController');
+const mainController = require('./controllers/MainController');
 
+router.get('/', mainController.healthCheck);
 router.post('/subscribe', notificationController.subscribe);
 router.post('/alert', notificationController.alert);
 
